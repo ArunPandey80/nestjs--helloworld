176368a6248584b1e5750efec1e381e053d0d7ee
AryaBaride <baridearya@gmail.com>
2024-06-17 19:26:58 +0530

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+// src/app.module.ts
+
+import { Module } from '@nestjs/common';
+import { HelloController } from './hello/hello.controller';
+
+@Module({
+  imports: [],
+  controllers: [HelloController],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/hello/hello.controller.ts
@@ -0,0 +1,11 @@
+// src/hello/hello.controller.ts
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('hello')
+export class HelloController {
+  @Get()
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
